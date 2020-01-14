d82a3609e5cacd185722637541a6da8bc6518e78
asd233 <2053044516@qq.com>
2020-01-14 21:39:17 +0800

    添加 用于处理scrollLeft/Top的兼容性函数

--- common.js
+++ common.js
@@ -1,3 +1,11 @@
 function my$(id) {
     return document.getElementById(id);
 }
+
+//用于处理scrollLeft/Top的兼容性函数
+function getscroll() {
+    var scrollLeft = document.body.scrollLeft || document.documentElement.scrollLeft;
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    return scrollLeft;
+    return scrollTop;
+}
