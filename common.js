bd3ae5563729211cb09c2b9ff9a8f2884ff4e432
asd233 <2053044516@qq.com>
2020-01-15 17:04:09 +0800

    添加 处理pageX/Y兼容性的函数

--- common.js
+++ common.js
@@ -9,3 +9,15 @@ function getscroll() {
     return scrollLeft;
     return scrollTop;
 }
+
+//处理pageX/Y兼容性的方法
+// pageX/Y = clientX/Y + 页面滚动的距离(document.body.scrollLeft/scrollTop)
+function getpage(e) {
+    var e = e || window.event;
+    var pageX = e.pageX || e.clientX + getscroll().scrollLeft;
+    var pageY = e.pageY || e.clientY + getscroll().scrollTop;
+    return {
+        pageX: pageX,
+        pageY: pageY
+    }
+}
