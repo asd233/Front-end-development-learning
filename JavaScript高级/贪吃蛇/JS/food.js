81331c36869ab2fbccd495d00c4173236dd1586e
asd233 <2053044516@qq.com>
2020-06-20 21:03:55 +0800

    优化创建对象时的样式逻辑

--- JavaScript高级/贪吃蛇/JS/food.js
+++ JavaScript高级/贪吃蛇/JS/food.js
@@ -2,36 +2,30 @@
     var arr = [];
     function Food(style) {
         style = style || {};
-        this.width = style.width || 25;
-        this.height = style.width || 25;
-        this.x = style.x || Tools.random(0, map.offsetWidth / this.width - 1) * this.width;
-        this.y = style.y || Tools.random(0, map.offsetHeight / this.height - 1) * this.height;
-        this.color = style.color || "green";
+        this.width = style.width || 25 + "px";
+        this.height = style.width || 25 + "px";
+        this.left = Tools.random(0, map.offsetWidth / parseInt(this.width) - 1) * parseInt(this.width) + "px";
+        this.top = Tools.random(0, map.offsetHeight / parseInt(this.height) - 1) * parseInt(this.height) + "px";
+        this.backgroundColor = style.backgroundColor || "#00ff00";
+        this.position = style.position || "absolute"
     }
 
     Food.prototype.render = function (map) {
         //此方法将Food渲染到页面上
-        remove();
-        //渲染前将页面已有的Food移除
-        var div = document.createElement("div");
-        arr.push(div);
-        map.appendChild(div);
-        this.x = Tools.random(0, map.offsetWidth / this.width - 1) * this.width;
-        this.y = Tools.random(0, map.offsetHeight / this.height - 1) * this.height;
-        div.style.left = this.x + "px";
-        div.style.top = this.y + "px";
-        div.style.position = "absolute";
-        div.style.width = this.width + "px";
-        div.style.height = this.height + "px";
-        div.style.backgroundColor = this.color;
-        div.setAttribute("id", "food");
-    }
-
-    function remove() {
         if (arr.length !== 0) {
             arr[0].remove();
             arr.splice(0, 1);
         }
+        //渲染前将页面已有的Food移除
+        var div = document.createElement("div");
+        arr.push(div);
+        this.left = Tools.random(0, map.offsetWidth / parseInt(this.width) - 1) * parseInt(this.width) + "px";
+        this.top = Tools.random(0, map.offsetHeight / parseInt(this.height) - 1) * parseInt(this.height) + "px";
+        for (const key in this) {
+            div.style[key] = this[key];
+        }
+        div.setAttribute("id", "food");
+        map.appendChild(div);
     }
     window.Food = Food;
     //将Food对象挂在window对象下使的外部可以访问Food对象
--- JavaScript高级/贪吃蛇/JS/snake.js
+++ JavaScript高级/贪吃蛇/JS/snake.js
@@ -1,25 +1,30 @@
 (function () {
     function Snake(style) {
         style = style || {};
-        this.width = style.width || 25;
-        this.height = style.height || 25;
+        this.width = style.width || 25 + "px";
+        this.height = style.height || 25 + "px";
         this.direction = style.direction || "right";
+        this.position = style.position || "absolute"
         this.body = [
-            { x: 3, y: 2, color: "red" },
-            { x: 2, y: 2, color: "green" },
-            { x: 1, y: 2, color: "green" },
+            { left: 3, top: 2, backgroundColor: "red" },
+            { left: 2, top: 2, backgroundColor: "green" },
+            { left: 1, top: 2, backgroundColor: "green" },
         ];
     }
     Snake.prototype.render = function (map) {
         for (var i = 0, length = this.body.length; i < length; i++) {
             var div = document.createElement("div");
             map.appendChild(div);
-            div.style.position = "absolute";
-            div.style.width = this.width + "px";
-            div.style.height = this.height + "px";
-            div.style.top = this.body[i].y * this.height + "px";
-            div.style.left = this.body[i].x * this.width + "px";
-            div.style.backgroundColor = this.body[i].color;
+            for (const key in this) {
+                div.style[key] = this[key];
+            }
+            for (const key in this.body[i]) {
+                if (key != "backgroundColor") {
+                    div.style[key] = this.body[i][key] * parseInt(this.width) + "px";
+                } else {
+                    div.style[key] = this.body[i][key];
+                }
+            }
         }
     }
     window.Snake = Snake;
