8918427eb881cecd564e6283f4b2d0c68311a8f6
asd233 <2053044516@qq.com>
2020-06-28 21:28:37 +0800

    添加gameOver方法;添加keydown事件,用于使用方向键控制方向。

--- JavaScript高级/贪吃蛇/JS/game.js
+++ JavaScript高级/贪吃蛇/JS/game.js
@@ -1,16 +1,57 @@
-(function (map) {
-    function Game() {
+(function () {
+    var that;
+    function Game(map) {
         this.food = new Food();
         this.snake = new Snake();
         this.map = map;
+        that = this;
     }
-    Game.prototype.start = function (map) {
-        this.food.render(map);
-        this.snake.render(map);
-        setInterval(() => {
+    Game.prototype.start = function () {
+        this.food.render(this.map);
+        this.snake.render(this.map);
+        this.moveId = setInterval(() => {
             this.snake.move();
-            this.snake.render(map);
+            this.snake.render(this.map);
+            for (let i = 1; i < this.snake.body.length; i++) {
+                if (this.snake.body[0].left == this.snake.body[i].left && this.snake.body[0].top == this.snake.body[i].top) {
+                    this.gameOver();
+                    break;
+                }
+            }
         }, 200);
+    }
+    Game.prototype.gameOver = function () {
+        clearInterval(this.moveId);
+    }
+    document.onkeydown = function (event) {
+        switch (event.keyCode) {
+            case 37:
+                // 向左
+                if (that.snake.direction != "right") {
+                    that.snake.direction = "left";
+                }
+                break;
+            case 38:
+                // 向上
+                if (that.snake.direction != "bottom") {
+                    that.snake.direction = "top";
+                }
+                break;
+            case 39:
+                // 向右
+                if (that.snake.direction != "left") {
+                    that.snake.direction = "right";
+                }
+                break;
+            case 40:
+                // 向下
+                if (that.snake.direction != "top") {
+                    that.snake.direction = "bottom"
+                }
+                break;
+            default:
+                break;
+        }
     }
     window.Game = Game;
 })()
